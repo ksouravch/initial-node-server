f98a5e904bb6780c428fc9e9ac1591bd047c9ea3
ksourav82 <ksourav_82@yahoo.co.in>
2018-09-30 02:49:54 +0530

    Setup for Heroku deployment

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ app.get('/', (req, res) => {
 	res.send("Hello World from Github");
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
 	console.log("Server is up and running");
 });
