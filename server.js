f45e13839d15317385a10172303a4f01c6b8e51a
ksourav82 <ksourav_82@yahoo.co.in>
2018-10-02 23:09:23 +0530

    Removing static contents and using handle bar to diaplay content of each page

--- server.js
+++ server.js
@@ -6,35 +6,59 @@ hbs.registerPartials(__dirname + '/views/partials');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-	res.render('home.hbs');
+	res.render('home.hbs', {
+		pageTitle: "Welcome",
+		pageContent: "Welcome to Home page"
+	});
 });
 
 app.get('/about', (req, res) => {
-	res.render('about.hbs');
+	res.render('about.hbs', {
+		pageTitle: "About Us",
+		pageContent: "Welcome to About page"
+	});
 });
 
 app.get('/dashboard', (req, res) => {
-	res.render('dashboard.hbs');
+	res.render('dashboard.hbs', {
+		pageTitle: "Dashboard",
+		pageContent: "Welcome to Dashboard page"
+	});
 });
 
 app.get('/shortcuts', (req, res) => {
-	res.render('shortcuts.hbs');
+	res.render('shortcuts.hbs', {
+		pageTitle: "Shortcuts",
+		pageContent: "Welcome to Shortcuts page"
+	});
 });
 
 app.get('/overview', (req, res) => {
-	res.render('overview.hbs');
+	res.render('overview.hbs', {
+		pageTitle: "Overview",
+		pageContent: "Welcome to Overview page"
+	});
 });
 
 app.get('/events', (req, res) => {
-	res.render('events.hbs');
+	res.render('events.hbs', {
+		pageTitle: "Events",
+		pageContent: "Welcome to Events page"
+	});
 });
 
 app.get('/services', (req, res) => {
-	res.render('services.hbs');
+	res.render('services.hbs', {
+		pageTitle: "Services",
+		pageContent: "Welcome to Services page"
+	});
 });
 
 app.get('/contact', (req, res) => {
-	res.render('contact.hbs');
+	res.render('contact.hbs', {
+		pageTitle: "Contact Us",
+		pageContent: "Welcome to Contact page"
+	});
 });
 
 
