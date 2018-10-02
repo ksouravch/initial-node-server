1bceb2dfe0908852fe63ab6f69e305f1f20dfdbe
ksourav82 <ksourav_82@yahoo.co.in>
2018-10-02 22:44:40 +0530

    Adding handlebar template engine to the project and changing the static files accordingly

--- server.js
+++ server.js
@@ -1,11 +1,43 @@
 const express = require('express');
+const hbs = require('hbs');
 const app = express();
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
 app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-	res.sendFile(__dirname + "/public/index.html");
+	res.render('home.hbs');
+});
+
+app.get('/about', (req, res) => {
+	res.render('about.hbs');
+});
+
+app.get('/dashboard', (req, res) => {
+	res.render('dashboard.hbs');
+});
+
+app.get('/shortcuts', (req, res) => {
+	res.render('shortcuts.hbs');
+});
+
+app.get('/overview', (req, res) => {
+	res.render('overview.hbs');
+});
+
+app.get('/events', (req, res) => {
+	res.render('events.hbs');
+});
+
+app.get('/services', (req, res) => {
+	res.render('services.hbs');
+});
+
+app.get('/contact', (req, res) => {
+	res.render('contact.hbs');
 });
 
 
 app.listen(process.env.PORT || 3000, () => {
-	console.log("Server is up and running");
+	console.log(`Server is up and running at ${process.env.PORT}`);
 });
