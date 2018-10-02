66c3d6542dbed51448a0568d2cdb7d2be9f5d0c6
ksourav82 <ksourav_82@yahoo.co.in>
2018-10-02 21:29:53 +0530

    Adding public foldr and creating simple static site

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+app.use(express.static('public'));
 app.get('/', (req, res) => {
-	res.send("Hello World from Github");
+	res.sendFile(__dirname + "/public/index.html");
 });
+
 
 app.listen(process.env.PORT || 3000, () => {
 	console.log("Server is up and running");
